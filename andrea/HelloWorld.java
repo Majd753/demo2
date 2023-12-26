5c1fd96c98c31cda11d838ba1746d166d8d5c4cb
Majd Zreid <mjdzreed@gmail.com>
2023-12-27 02:43:17 +0300

    try

--- andrea/HelloWorld.java
+++ andrea/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, World! more");
         for (int i = 0; i < 6; i++) {
             System.out.println("siiiiii");
         } 
