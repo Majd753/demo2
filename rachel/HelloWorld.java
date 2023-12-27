c00b02c3c7176d1510a1716adb6065da4d1000d0
Majd Zreid <mjdzreed@gmail.com>
2023-12-27 04:02:56 +0300

    change more

---
+++ rachel/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! more");
+        for (int i = 0; i < 6; i++) {
+            System.out.println("siiiiii");
+        } 
+        String[] array = new String[3];
+        array[0] = "Ahmed";
+        array[1] = "Moh";
+        array[2] = "Khaled";
+        for (String s : array) {
+            System.out.println(s);
+        }
+    }
+}
